f43799a143531606e97ac8f6a6d239b1d8b0bf6c
Crepiks <sayazhan.onlassyn@mail.ru>
2021-11-02 08:01:41 +0600

    feat: install and set up apexcharts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
+import VueApexCharts from "vue-apexcharts";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/assets/scss/main.scss";
+
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 
 Vue.config.productionTip = false;
 
