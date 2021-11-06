99234bf5826331bdcbe5d3c4aeba42d4192cecdf
Crepiks <sayazhan.onlassyn@mail.ru>
2021-11-06 21:45:11 +0600

    feat: implement CommonStatistics component

---
+++ src/components/common/CommonStatistics/CommonStatistics.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="statistics">
+    <div class="statistics__icon-wrapper">
+      <i :class="`bx bx-${icon} statistics__icon`" />
+    </div>
+    <div class="statistics__column">
+      <span class="statistics__name">{{ name }}</span>
+      <span class="statistics__value">{{ value }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: "user",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.statistics {
+  padding: 14px;
+  display: flex;
+  border-radius: 10px;
+  background-color: white;
+}
+
+.statistics__icon-wrapper {
+  width: 60px;
+  height: 60px;
+  margin-right: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fef4df;
+  border-radius: 50%;
+}
+
+.statistics__icon {
+  font-size: 32px;
+  color: $primary-color;
+}
+
+.statistics__column {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.statistics__name {
+  margin-bottom: 2px;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.6);
+}
+
+.statistics__value {
+  font-size: 26px;
+  font-weight: 500;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="dashboard">
+    <div class="dashboard__statistics">
+      <common-statistics
+        icon="credit-card-front"
+        name="Доход"
+        value="46 000 ₸"
+      />
+      <common-statistics icon="user" name="Регистрации" value="463" />
+      <common-statistics icon="book" name="Пройдено уроков" value="1246" />
+    </div>
     <users-chart class="dashboard__users-chart" />
     <div class="dashboard__row">
       <div>
@@ -11,12 +20,14 @@
 </template>
 
 <script>
+import CommonStatistics from "@/components/common/CommonStatistics/CommonStatistics";
 import UsersChart from "@/components/dashboard/UsersChart/UsersChart";
 import TransactionsList from "@/components/dashboard/TransactionsList/TransactionsList";
 import PaymentsChart from "@/components/dashboard/PaymentsChart/PaymentsChart";
 
 export default {
   components: {
+    "common-statistics": CommonStatistics,
     "users-chart": UsersChart,
     "transactions-list": TransactionsList,
     "payments-chart": PaymentsChart,
@@ -27,6 +38,13 @@ export default {
 <style scoped>
 .dashboard {
   padding: 30px;
+}
+
+.dashboard__statistics {
+  margin-bottom: 30px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 30px;
 }
 
 .dashboard__users-chart {
